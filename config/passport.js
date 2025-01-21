1d1a43bc6382fdd67e48ec9296cba45939c141f3
Prasanna <pgurung1@my.hpu.edu>
2025-01-20 18:50:49 -0600

    updated gitignore

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const LocalStrategy = require('passport-local').Strategy
+const mongoose = require('mongoose')
+const User = require('../model/User')
+
+module.exports = function (passport) {
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+   User.findOne({ email: email.toLowerCase()})
+   .then((user)=>{
+    if (!user) {
+      return done(null, false, { msg: `Email ${email} not found.` })
+    }
+    if (!user.password) {
+      return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' })
+    }
+    user.comparePassword(password, (err, isMatch) => {
+      if (err) { return done(err) }
+      if (isMatch) {
+        return done(null, user)
+      }
+      return done(null, false, { msg: 'Invalid email or password.' })
+    })
+
+   })
+   .catch((err)=>{
+    console.log(err)
+   })
+
+ }))
+
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+   
+  })
+
+
+  passport.deserializeUser(function(id, done) { process.nextTick(function() { return done(null, id); }); });
+}
